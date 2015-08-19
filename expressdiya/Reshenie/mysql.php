4d7c761db85c4b5093e74369d8e4e480939ff882
Alexander <a89good@gmail.com>
2015-08-19 19:05:59 +0300

    new commit

--- expressdiya/Reshenie/mysql.php
+++ expressdiya/Reshenie/mysql.php
@@ -1,7 +1,83 @@
+<html>
+    <head>
+        <title>MYSql</title>
+        <style>
+            thead{
+                font-weight: bold;
+                font-size: 20px;
+            }
+
+            td{
+                padding: 10px;
+                border: 2px solid #000000;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+
 <?php
 /**
  * Created by PhpStorm.
  * User: Alexandr
  * Date: 18.08.2015
  * Time: 20:59
- */ 
+ */
+
+$dbh = new PDO('mysql:host=localhost;dbname=expressdiya', 'root', '');
+
+$result = $dbh->query('
+      SELECT
+        categories.name as cName,
+        P.productCount,
+        P.minPrice,
+        P.maxPrice,
+        products.name,
+        P.lengthMaxDescription,
+        products.description
+      FROM categories
+      INNER JOIN products ON products.category_id = categories.id
+      INNER JOIN
+      (SELECT
+        COUNT(*) as productCount,
+        MIN(products.price) as minPrice,
+        MAX(products.price) as maxPrice,
+        MAX(LENGTH(products.description)) as lengthMaxDescription
+        FROM products
+        GROUP BY products.category_id
+      ) AS P ON P.lengthMaxDescription = LENGTH(products.description)
+      GROUP BY  categories.name
+    ', PDO::FETCH_ASSOC);
+
+echo '<table>
+            <thead>
+                <tr>
+                    <td>Категория</td>
+                    <td>Количество</td>
+                    <td>Мин. цена</td>
+                    <td>Макс. цена</td>
+                    <td>Название товара с самым длинным описанием</td>
+                    <td>Длина самого длинного описания</td>
+                    <td>Самое длинное описание</td>
+                </tr>
+            </thead>
+            <tbody>';
+
+foreach($result as $row) {
+    echo '<tr>
+                <td>'.$row["cName"].'</td>
+                <td>'.$row["productCount"].'</td>
+                <td>'.$row["minPrice"].'</td>
+                <td>'.$row["maxPrice"].'</td>
+                <td>'.$row["name"].'</td>
+                <td>'.$row["lengthMaxDescription"].'</td>
+                <td>'.$row["description"].'</td>
+            </tr>';
+}
+
+echo '</tbody></table>';
+
+?>
+
+    </body>
+</html>
--- expressdiya/Reshenie/php.php
+++ expressdiya/Reshenie/php.php
@@ -4,4 +4,107 @@
  * User: Alexandr
  * Date: 19.08.2015
  * Time: 12:04
- */ 
+ */
+
+/*
+ * В принципе, я нашел решение абсолютно идентичного задания вот по этой
+ * ссылке https://gist.github.com/josecanciani/ff535426bd5d453ef9c2
+ * За исключением раскрашивания клеток. Но руководствуясь принципом
+ * "Велосепедостроения" я соорудил свой собственный "Самокат" с бэкджэком
+ * и продажной любовью:)
+ * */
+
+class AsciiTabel{
+    const S_PLUS = '+';
+    const S_MINUS = '-';
+    const S_VLINE = '|';
+    const N_SPACES = 2;
+
+    protected $_hSpace = '';
+    public  $_maxLength = [];
+
+    public $array = [];
+    public $hLine = '';
+
+    public function __construct($array){
+        $this ->_hSpace = str_repeat('&nbsp;', self::N_SPACES);
+        $this ->array = $array;
+        $this -> maxLengthInColumn($array);
+        $this ->createLine();
+    }
+
+    protected function maxLengthInColumn($array){
+        foreach($array[0] as $k => $v){
+            $this -> _maxLength[$k] = strlen($k);
+        }
+
+        foreach($array as $v){
+            foreach($this -> _maxLength as $k2 => $v2){
+                if(strlen($v[$k2]) > $v2){
+                    $this -> _maxLength[$k2] = strlen($v[$k2]);
+                }
+            }
+        }
+    }
+
+    protected function createLine(){
+        foreach($this -> _maxLength as $k => $v){
+            $this -> hLine .= self::S_PLUS;
+            $this -> hLine .= str_repeat(self::S_MINUS, self::N_SPACES * 2 + $v);
+        }
+        $this -> hLine .= self::S_PLUS.'<br>';
+    }
+
+    protected function createHeader(){
+        $header = $this ->hLine;
+        foreach($this -> _maxLength as $k => $v){
+            $odd = ($v - strlen($k)) % 2;
+            $newHSpace = ($v - strlen($k) - $odd) / 2 + self::N_SPACES;
+            $header .= self::S_VLINE . str_repeat('&nbsp;', $newHSpace) . $k . str_repeat('&nbsp;', $newHSpace + $odd);
+        }
+        return $header . self::S_VLINE.'<br>'.$this ->hLine;
+    }
+
+    protected function createBody(){
+        $body = '';
+        foreach($this->array as $v){
+            foreach($this -> _maxLength as $k2 => $v2){
+                $sSpan = '';
+                $cSpan = '';
+                if($k2 === 'Color'){
+                    $sSpan = '<span style="background-color: '.$v[$k2].'; margin:-6px -3px -7.5px; padding: 6px 3px 7.5px; display:inline-block">';
+                    $cSpan = '</span>';
+                }
+                $body .= self::S_VLINE .$sSpan. $this ->_hSpace . $v[$k2] . str_repeat('&nbsp;', $v2 - strlen($v[$k2]) + self::N_SPACES).$cSpan;
+            }
+            $body .= self::S_VLINE.'<br>'.$this ->hLine;
+        }
+        return $body;
+    }
+
+    public function drawAsciiTabel(){
+        echo '<pre>' . $this ->createHeader() . $this ->createBody() . '</pre>';
+    }
+}
+
+
+$arr = array (
+    array (
+        'Name' => 'Trixie',
+        'Color' => 'Green',
+        'Element' => 'Earth',
+        'Likes' => 'Flowers'),
+    array (
+        'Name' => 'Tinkerbell',
+        'Element' => 'Air',
+        'Likes' => 'Singning',
+        'Color' => 'Blue'),
+    array (
+        'Name' => 'Blum',
+        'Element' => 'Water',
+        'Likes' => 'Dancing',
+        'Color' => 'Pink'),
+);
+
+$obj = new AsciiTabel($arr);
+$obj ->drawAsciiTabel();
